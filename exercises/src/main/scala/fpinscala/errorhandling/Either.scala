b35dce55cac652ea92402d4c30ef193fa14927b7
Bryan Castillo <castillo.bryan@gmail.com>
2015-06-27 08:56:07 -0700

    Implementing basic functions/methods for Either.

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,15 +4,28 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+  
+ def map[B](f: A => B): Either[E, B] = this match {
+   case Right(value) => Right(f(value))
+   case l: Left[E] => l
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case Right(value) => f(value)
+   case l: Left[E] => l
+ }
+   
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case r: Right[A] => r
+   case l: Left[E]  => b
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
-
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+   for (i1 <- this; i2 <- b) yield f(i1, i2)
 }
+
 case class Left[+E](get: E) extends Either[E,Nothing]
+
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
---
+++ exercises/src/main/scala/fpinscala/errorhandling/EitherTest.scala
@@ -0,0 +1,51 @@
+package fpinscala.errorhandling
+
+import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+
+object EitherTest {
+  
+  def main(args: Array[String]): Unit = {
+    
+    println({
+      val item: Either[String, Int] = Left("Oh nose")
+      item.map(i => i * 2)
+    })
+    
+    println({
+      val item: Either[String, Int] = Right(2)
+      item.map(i => i * 2)
+    })
+    
+    {
+      def flatMapTest(item1: Either[String, Int], item2: Either[String, Int]): Unit = {
+        println(item1.flatMap { i1 => item2.map { i2 => i1 + i2 } })
+      }
+      
+      flatMapTest(Right(1), Right(2))
+      flatMapTest(Left("#1 is no good."), Right(2))
+      flatMapTest(Right(1), Left("#2 failed!"))
+      flatMapTest(Left("Boom!"), Left("Project #1 failed before me, so I'm good?"))
+    }
+
+    println(Right("Extend claws").orElse(Right("Run")))
+    println(Left("Hydraulic motor failure").orElse(Right("Headbutt")))
+    
+    {
+      def tryTrick(trick: String, failureMode: String): Either[String, String] =
+        if (math.random <= 0.5)
+          Right(trick)
+        else
+          Left(failureMode)
+      
+      for (n <- 1 to 10)
+        println(
+          tryTrick("Kickflip", "Biff")
+            .map2(tryTrick("Rail slide", "Crunch")) { (t1, t2) =>
+              s"$t1 to $t2! Whoa 100 points!"
+            })
+          
+    }
+    
+  }
+  
+}
