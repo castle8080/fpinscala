8643d62c1242faf35709d6ea5fb9e2b81d91aed4
Bryan Castillo <castillo.bryan@gmail.com>
2015-07-14 07:37:40 -0700

    Moving code back into the main Gen class.

--- exercises/src/main/scala/fpinscala/testing/Exercises_GenFirstDraft.scala
+++ exercises/src/main/scala/fpinscala/testing/Exercises_GenFirstDraft.scala
@@ -4,27 +4,6 @@ import fpinscala.state._
 
 object Exercises_GenFirstDraft {
   
-  case class Gen[A](sample: State[RNG,A])
-  
-  object Gen {
-    
-    def choose(start: Int, stopExclusive: Int): Gen[Int] =
-      Gen(State(RNG.nonNeagativeBetween(start, stopExclusive)))
-
-    def unit[A](a: => A): Gen[A] =
-      Gen(State(RNG.unit(a)))
-    
-    def boolean: Gen[Boolean] =
-      Gen(State(RNG.boolean))
-    
-    def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
-      sequence(List.fill(n)(g))
-
-    def sequence[A](gens: List[Gen[A]]): Gen[List[A]] =
-      Gen(State.sequence(gens.map(_.sample)))
-      
-  } 
-  
   def main(args: Array[String]): Unit = {
     println(Gen.listOfN(10, Gen.boolean).sample.run(new RNG.Simple(System.currentTimeMillis)))
     println(Gen.listOfN(10, Gen.choose(1, 3)).sample.run(new RNG.Simple(System.currentTimeMillis)))  
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -17,10 +17,11 @@ trait Prop {
   
   def check: Boolean
   
-  def &&(p: Prop): Prop =
+  def &&(p: Prop): Prop = {
     new Prop {
       def check: Boolean = Prop.this.check && p.check
     }
+  }
   
 }
 
@@ -29,10 +30,25 @@ object Prop {
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+
+  def unit[A](a: => A): Gen[A] =
+    Gen(State(RNG.unit(a)))
+  
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNeagativeBetween(start, stopExclusive)))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.boolean))
+    
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    sequence(List.fill(n)(g))
+
+  def sequence[A](gens: List[Gen[A]]): Gen[List[A]] =
+    Gen(State.sequence(gens.map(_.sample)))
+
 }
 
-trait Gen[A] {
+case class Gen[+A](sample: State[RNG,A]) {
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 }
